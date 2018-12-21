039d6dfa5474a7f681d9954963f283990a1eab26
tanqianshi <tanqianshi001@lianjia.com>
2018-12-21 21:47:06 +0800

    sql逻辑

--- server/src/Action/login.js
+++ server/src/Action/login.js
@@ -7,25 +7,26 @@ exports.index = {
     needLogin: false,
     cors: false,//是否允许跨域访问  支持[域名1，域名形式]  注：功能还未增加
     requestHandler: async ctx => {
-        let { login } = Config;
-        let { query } = ctx.request
-        //
-        // ctx.cookies.set(CONFIG.login.tokenName, '2.0069dd613afce9619e7874df886263a8cc')
-        // return ctx.redirect('/')
-        // console.log(query)
-        return ctx.ajax({ code: 1, data: ctx.user })
-        //let { login } = CONFIG
 
-        //
+        let { login } = Config
+        let { query, cookies } = ctx
+        //let token = ctx.cookies.get(login.tokenName); //基于sso设置的cookie
+        let token = ctx.query[login.ticketKey]
+        cookies.set(login.tokenName, 'abcd');//token)
+        if (token) {
+            //console.log(ctx.cookies.get(login.tokenName),'1212121')
+            //再一次检测token是否合法
+            await Modal.checkLogin.check(ctx, token)
+            if (ctx.user) {
 
-        if (ctx.query[login.backToken]) {
-            ctx.cookies.set(login.tokenName, '2.0052efe3acc7dbe30843465d1edc2f6667')
+                return ctx.redirect(query.goto)
+            }
+
 
             return ctx.ajax({ code: 1, data: ctx.user })
         } else {
             let url = login.ssoUrl + login.ssoLogin + '?' + login.backQueryName + '=' + 'http://beta.mp.lianjia.com:3200'
 
-            return ctx.redirect(url)
         }
 
         // http://passport.lianjia.com/cas/login?service=http://beta.mp.lianjia.com:3300/login?gotoURL=%252F
